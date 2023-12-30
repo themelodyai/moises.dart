b48d693c2e8a013de772b8d28de18bc0f3f688de
Bruno D'Luka <brunodlukaa@gmail.com>
2023-12-29 23:35:52 -0300

    feat: Tests

--- test/music_test.dart
+++ test/music_test.dart
@@ -2,5 +2,27 @@ import 'package:moises_dart/music.dart';
 import 'package:test/test.dart';
 
 void main() {
-  group('A group of tests', () {});
+  group('Job', () {
+    test('Job parses correctly from the server', () {
+      final json = {
+        'id': 'job-id',
+        'name': 'job-name',
+        'status': 'succeeded',
+        'workflow': {
+          'id': 'workflow-id',
+          'name': 'workflow-name',
+        },
+        'workflowParams': {'key': 'value'},
+      };
+
+      final job = Job.fromJson(json);
+
+      expect(job.id, 'job-id');
+      expect(job.name, 'job-name');
+      expect(job.status, JobStatus.succeeded);
+      expect(job.workflow.$1, 'workflow-id');
+      expect(job.workflow.$2, 'workflow-name');
+      expect(job.workflow.$3, {'key': 'value'});
+    });
+  });
 }
