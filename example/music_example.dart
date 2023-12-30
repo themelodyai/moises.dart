82a35ff9d4e22cb01ce1fde7ba3fbfc0974ce82c
Bruno D'Luka <brunodlukaa@gmail.com>
2023-12-29 23:33:14 -0300

    feat: Example code

--- example/music_example.dart
+++ example/music_example.dart
@@ -1,3 +1,27 @@
-import 'package:music_ai_dart/music.dart';
+import 'dart:io';
 
-void main() {}
+import 'package:moises_dart/api.dart';
+import 'package:moises_dart/file.dart';
+
+void main() async {
+  final apiKey = String.fromEnvironment('moises_api_key');
+  final client = MusicAiClient(apiKey: apiKey);
+
+  final file = File('path/to/file.mp3');
+  final bytes = await file.readAsBytes();
+  final downloadUrl = await client.uploadFile(bytes);
+
+  if (downloadUrl != null) {
+    final jobId = await client.createJob(
+      'Generate lyrics', // any name
+      'generate-lyrics', // the workflow name you created
+      {'inputUrl': downloadUrl},
+    );
+
+    await Future.delayed(Duration(seconds: 5));
+
+    final job = await client.getJob(jobId);
+
+    print('Job created: ${job.id}');
+  }
+}
